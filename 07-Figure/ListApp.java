a63c4843041ab41201caa9d11ab7f7a74dc73e4e
Peixinho20 <ctlcamila45@gmail.com>
2021-10-18 17:38:34 -0300

    07-Figure V2

--- 07-Figure/ListApp.java
+++ 07-Figure/ListApp.java
@@ -34,22 +34,14 @@ class ListFrame extends JFrame {
                     int w = rand.nextInt(50);
                     int h = rand.nextInt(50);
                     if (evt.getKeyChar() == 'r') {
-                        Rect r = new Rect(x,y, w,h);
-                        figs.add(r);
+                        figs.add(new Rect(x,y, w,h));
                     } else if (evt.getKeyChar() == 'e') {
                         figs.add(new Ellipse(x,y, w,h));
-                    } else {
-                    	int x1 = rand.nextInt(350);//tamanho janela
-		                int y1 = rand.nextInt(350);//tamanho janela
-		                int x2 = rand.nextInt(350);//tamanho janela
-		                int y2 = rand.nextInt(350);//tamanho janela 
-		                int x3 = rand.nextInt(350);//tamanho janela
-		                int y3 = rand.nextInt(350);//tamanho janela 
-                    	if (evt.getKeyChar() == 't'){
-                    		figs.add(new Triangle(x1,y1,x2,y2,x3,y3));
-                    	}
-                      
-                    }
+                    } else if (evt.getKeyChar() == 't'){
+                    	figs.add(new Triangle(x,y, w,h));                      
+                    }/* else if (evt.getKeyChar() == 's'){
+                    	figs.add(new Star(x,y, w,h));                      
+                    }*/
                    
                     repaint();
                 }
