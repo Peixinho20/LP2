8e0a0ab7d5d07743a98bd50da644f09e8a52a096
Peixinho20 <ctlcamila45@gmail.com>
2021-10-19 11:04:20 -0300

    reescrevendo

--- Projeto/ProjectApp.java
+++ Projeto/ProjectApp.java
@@ -1,192 +1,72 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.Color;
-import java.awt.event.MouseEvent;
 import javax.swing.*;
-import javax.swing.JFrame;
 import java.util.ArrayList;
-import javax.swing.JPanel;
-import java.awt.Toolkit;
+import java.util.Random;
+
 import figures.*;
 
 class ProjectApp {
     public static void main (String[] args) {
         ListFrame frame = new ListFrame();
         frame.setVisible(true);
-        
     }
 }
 
-class ListFrame extends JFrame {
+class ListFrame extends JFrame { //listas de figuras ativas
     ArrayList<Figure> figs = new ArrayList<Figure>();
-    Figure focus = null;
-    Point pMouse = null;
-
-    int dx,dy;
-    int contcontorno=1;
-    int contpreenchimento=0;
-    Color cores[] = {Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.MAGENTA, Color.CYAN, Color.WHITE, Color.BLACK, Color.GRAY, Color.LIGHT_GRAY, Color.DARK_GRAY, Color.PINK, Color.ORANGE};
+    Random rand = new Random();
+    
+    Figure focus = null; //figura em foco no momento
 
     ListFrame () {
-        this.addWindowListener (
+        this.addWindowListener ( //adcionar ouvinte de janela
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
                     System.exit(0);
                 }
             }
         );
-        this.addMouseListener(
-            new MouseAdapter(){
-                public void mousePressed(MouseEvent evt){
-                    try{
-                        int x = evt.getX();
-                        int y = evt.getY();
-                        pMouse = getMousePosition();
-                        focus = null;
-                        for (Figure fig: figs){
-                            if (fig.clicked(pMouse.x,pMouse.y)){
-                                focus = fig;
-                                dx = focus.x - pMouse.x;
-                                dy = focus.y - pMouse.y;
-                            }
-                        }
-                        if (focus!=null){
-                            figs.remove(focus);
-                            figs.add(focus);
-                        }
-                        repaint();
-                        
-                    }catch(Exception x){}
-                }
-            }
-        );
         this.addMouseMotionListener(
-            new MouseMotionAdapter() {
-                public void mouseDragged(MouseEvent me) {
-                    pMouse = getMousePosition();
-                    if (focus != null) {
-                        figs.remove(focus);
-                        figs.add(focus);
-                        focus.x = pMouse.x + dx;
-                        focus.y = pMouse.y + dy;
+        	public void mousePressed (MouseEvent evt){
+        	}
+        );
+        
+        this.addMouseMotionListener(
+        	public void mouseDragged (MouseEvent evt){
+        	}
+        );
+
+        this.addKeyListener ( //adcionar ouvinte de teclado
+            new KeyAdapter() {
+                public void keyPressed (KeyEvent evt) {
+                    int x = rand.nextInt(350);
+                    int y = rand.nextInt(350);
+                    int w = rand.nextInt(50);
+                    int h = rand.nextInt(50);
+                    if (evt.getKeyChar() == 'r') {
+                        figs.add(new Rect(x,y, w,h));
+                    } else if (evt.getKeyChar() == 'e') {
+                        figs.add(new Ellipse(x,y, w,h));
+                    } else if (evt.getKeyChar() == 't'){
+                    	figs.add(new Triangle(x,y, w,h));                      
+                    } else if (evt.getKeyChar() == 's'){
+                    	figs.add(new Star(x,y, w,h));                      
                     }
+                   
                     repaint();
                 }
             }
         );
-        this.addKeyListener (
-            new KeyAdapter() {
-                public void keyPressed (KeyEvent evt) {
-                    pMouse = getMousePosition();
-                    int x = pMouse.x;
-                    int y = pMouse.y;
-                    int w = 50;
-                    int h = 50;
-                    Color contorno = cores[contcontorno];
-                    Color preenchimento = cores[contpreenchimento];
-                        
-                    if (evt.getKeyChar() == 'r') {
-                        Rect r = new Rect(x,y, w,h,Color.red,Color.black);
-                        figs.add(r);
 
-                        
-                    }
-                    else if (evt.getKeyChar() == 'e') {
-                        figs.add(new Ellipse(x,y,w,h,Color.blue,Color.yellow));
-                       
-                    }
-                    else if (evt.getKeyChar() == 't'){
-                        figs.add(new Triangle(x,y,w,h,Color.green,Color.black));
-                        
-                    }
-                    else if (evt.getKeyChar() == 's'){
-                        figs.add(new Star(x,y,w,h,Color.pink,Color.black));
-                        
-                    }
-                    try{
-                        if (evt.getKeyCode() == 10){
-                            for( Figure fig: figs){
-                                if ((focus == null) || (focus!=null)){
-                                    focus=fig;
-                                    figs.remove(focus);
-                                    figs.add(focus);
-                                    break;
-                                }
-                                repaint();
-                            
-
-                            }
-                            
-                        }
-                        else if(evt.getKeyCode() == 127){
-                            figs.remove(focus);
-                            focus=null;
-
-                        }
-                        else if(evt.getKeyCode() == 38){
-                            focus.y-=10;
-                        }
-                        else if(evt.getKeyCode() == 37){
-                            focus.x-=10;
-                        }
-                        else if(evt.getKeyCode() == 40){
-                            focus.y+=10;
-                        }
-                        else if(evt.getKeyCode() == 39){
-                            focus.x+=10;
-                        }
-                        else if (evt.getKeyChar()=='a'){
-                            focus.w+=5;
-                            focus.h+=5;
-                        }
-                        else if (evt.getKeyChar()=='d'){
-                            if(focus.w>=10 && focus.h>=10){
-                                focus.w-=5;
-                                focus.h-=5;
-                            }
-                        }
-                        else if(evt.getKeyChar()=='c'){
-                            if (contcontorno==12){
-                                contcontorno=0;
-                            }
-                            else {
-                                contcontorno++;
-                            }
-                            focus.contorno=cores[contcontorno];
-
-                        }
-                        else if (evt.getKeyChar()=='p'){
-                            if (contpreenchimento==12){
-                                contpreenchimento=0;
-                            }
-                            else {
-                                contpreenchimento++;
-                            }
-                            focus.preenchimento=cores[contpreenchimento];
-
-                        }
-                        repaint();    
-                    }catch(Exception e){}
-                }
-            }
-        );
-
-        this.setTitle("Projeto");
+        this.setTitle("Lista de Figuras");
         this.setSize(350, 350);
     }
-    public void paint (Graphics g){
+
+    public void paint (Graphics g) { //redesnhar as figuras
         super.paint(g);
-        for(Figure fig: this.figs){
-            if(focus==fig){
-                focus.preencherFig(g);
-                focus.desenharBorda(g);
-            }
+        for (Figure fig: this.figs) {
             fig.paint(g);
         }
-        
-        
-    
     }
-
-    
 }
--- Projeto/figures/Rect.java
+++ Projeto/figures/Rect.java
@@ -2,24 +2,21 @@ package figures;
 
 import java.awt.*;
 
-public class Rect extends Figure{
-    public Rect (int x,int y,int w,int h,Color preencheCor,Color corBorda) {
-        super(x,y,w,h,preencheCor,corBorda);
+public class Rect extends Figure{ //construtor do retangulo
+    public Rect (int x,int y,int w,int h) {
+        super(x,y,w,h);
     }
 
     public void print () {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+            this.x, this.y, this.w, this.h);
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(this.preencheCor);
-        g2d.fillRect(this.x,this.y,this.w,this.h);
-        g2d.setColor(this.corBorda);
+        //g2d.setColor(this.preencheCor);
+        //g2d.fillRect(this.x,this.y,this.w,this.h);
+        //g2d.setColor(this.corBorda);
         g2d.drawRect(this.x,this.y, this.w,this.h);
-        
-        
-
     }
 }
