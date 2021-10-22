e7b8585253f2231a8a22d971dd0d60630e93e92c
Peixinho20 <ctlcamila45@gmail.com>
2021-10-22 17:21:17 -0300

    v3

--- Projeto2/figures/Triangle.java
+++ Projeto2/figures/Triangle.java
@@ -3,22 +3,31 @@ package figures;
 import java.awt.*;
 
 public class Triangle extends Figure {
-    public Triangle(int x,int y,int w,int h,Color preencheCor,Color corBorda) {
-        super(x,y,w,h,preencheCor,corBorda);
+    public Triangle(int x,int y,int w,int h) {
+        super(x,y,w,h);
     }
+    public Polygon polygon;
 
-    public void Print() {
+    public void print() {
         System.out.format("Triangulo de vertices (%d, %d), (%d, %d) e (%d, %d).\n",
             this.x, this.y, this.w, this.h);
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        int x1[] = {this.x, ((this.w)/2) + (this.x), (this.w) + (this.x)};
-        int y1[] = {this.y + h, (this.y + h) - (this.h), this.y + h};
-        g2d.setColor(this.preencheCor);
-        g2d.fillPolygon(x1, y1, 3); 
-        g2d.setColor(this.corBorda);
-        g2d.drawPolygon(x1, y1, 3);
+        
+        int x[] = {this.x, this.w/2 + this.x, this.w + this.x};
+        int y[] = {this.y + h, this.y, this.y + this.h};
+        Polygon polygon = new Polygon(x,y,3);
+        this.polygon = polygon;
+        Polygon Triangle = new Polygon(x, y, 3);
+        //g2d.setColor(this.preencheCor);
+        //g2d.fillPolygon(x, y, 3); 
+        //g2d.setColor(this.corBorda);
+        //g2d.drawPolygon(x, y, 3);
+        g2d.drawPolygon(this.polygon);
+        g2d.drawPolygon(this.polygon);   
     }
 }
+
+
